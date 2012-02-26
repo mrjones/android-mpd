027842e7d25bf7132b6db4e7a756583d2609db7f
Matt Jones <jonesmr+github@gmail.com>
2012-02-26 20:04:28 +0000

    hooked up to JavaMPD lib

--- src/es/mrjon/androidmpd/AndroidMpdClient.java
+++ src/es/mrjon/androidmpd/AndroidMpdClient.java
@@ -2,14 +2,24 @@ package es.mrjon.androidmpd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import org.bff.javampd.MPD;
+import org.bff.javampd.exception.MPDConnectionException;
 
-public class AndroidMpdClient extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+public class AndroidMpdClient extends Activity {
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    try {
+      MPD mpd = new MPD("192.168.1.100", 6600);
+      Log.v("AndroidMpdClient", "Version:" + mpd.getVersion());
+      Log.v("AndroidMpdClient", "Uptime:" + mpd.getUptime());
+      mpd.close();
+//    } catch(MPDConnectionException e) {
+    } catch(Exception e) {
+      Log.e("Android Mpd Client", "Error Connecting: " + e.getMessage());
     }
+  }
 }
