245a2deab9d8ccca201d362da740873bfa747d88
Matt Jones <jonesmr+github@gmail.com>
2012-02-26 19:37:24 +0000

    android create project

---
+++ src/es/mrjon/androidmpd/AndroidMpdClient.java
@@ -0,0 +1,15 @@
+package es.mrjon.androidmpd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidMpdClient extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
