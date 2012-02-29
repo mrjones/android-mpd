b90267acfca2837251b31f515e1d1a5a7820a3a1
Matt Jones <jonesmr+github@gmail.com>
2012-02-29 03:27:09 +0000

    added playlist info (not updated properly)

--- src/es/mrjon/androidmpd/AndroidMpdClient.java
+++ src/es/mrjon/androidmpd/AndroidMpdClient.java
@@ -7,7 +7,9 @@ import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import org.bff.javampd.MPD;
 import org.bff.javampd.exception.MPDConnectionException;
@@ -30,9 +32,25 @@ public class AndroidMpdClient extends Activity {
       mpd = new MPD("192.168.1.100", 6600);
       Log.v("AndroidMpdClient", "Version:" + mpd.getVersion());
       Log.v("AndroidMpdClient", "Uptime:" + mpd.getUptime());
+
+      ListView playListView = (ListView) findViewById(R.id.play_list_view);
+      List<String> playList = new ArrayList<String>();
+
+      for (MPDSong song : mpd.getMPDPlaylist().getSongList()) {
+        String rowContents = String.format("%s - %s", song.getArtist(), song.getTitle());
+        playList.add(rowContents);
+        Log.v("AndroidMpdClient", "Appening playlist item: " + rowContents);
+      }
+
+//      setListAdapter(new ArrayAdapter<String>(this, R.layout.play_list_view, playList));
+      playListView.setAdapter(
+        new ArrayAdapter<String>(this, R.layout.playlist_row, playList));
+
     } catch(Exception e) {
       Log.e("AndroidMpdClient - EXCEPTION", "Error Connecting: " + e.getMessage());
     }
+
+    
 
 
     Button playButton = (Button) findViewById(R.id.play_button);
@@ -45,21 +63,41 @@ public class AndroidMpdClient extends Activity {
           }
         }
       });
-    Button stopButton = (Button) findViewById(R.id.stop_button);
-    stopButton.setOnClickListener(new OnClickListener() {
-        public void onClick(View v) {
-          try {
-            mpd.getMPDPlayer().stop();
-          } catch(Exception e) {
-            Log.e("AndroidMpdClient - EXCEPTION", "Error Connecting: " + e.getMessage());
-          }
-        }
-      });
+    // Button stopButton = (Button) findViewById(R.id.stop_button);
+    // stopButton.setOnClickListener(new OnClickListener() {
+    //     public void onClick(View v) {
+    //       try {
+    //         mpd.getMPDPlayer().stop();
+    //       } catch(Exception e) {
+    //         Log.e("AndroidMpdClient - EXCEPTION", "Error Connecting: " + e.getMessage());
+    //       }
+    //     }
+    //   });
     Button pauseButton = (Button) findViewById(R.id.pause_button);
     pauseButton.setOnClickListener(new OnClickListener() {
         public void onClick(View v) {
           try {
             mpd.getMPDPlayer().pause();
+          } catch(Exception e) {
+            Log.e("AndroidMpdClient - EXCEPTION", "Error Connecting: " + e.getMessage());
+          }
+        }
+      });
+    Button backButton = (Button) findViewById(R.id.back_button);
+    backButton.setOnClickListener(new OnClickListener() {
+        public void onClick(View v) {
+          try {
+            mpd.getMPDPlayer().playPrev();
+          } catch(Exception e) {
+            Log.e("AndroidMpdClient - EXCEPTION", "Error Connecting: " + e.getMessage());
+          }
+        }
+      });
+    Button forwardButton = (Button) findViewById(R.id.forward_button);
+    forwardButton.setOnClickListener(new OnClickListener() {
+        public void onClick(View v) {
+          try {
+            mpd.getMPDPlayer().playNext();
           } catch(Exception e) {
             Log.e("AndroidMpdClient - EXCEPTION", "Error Connecting: " + e.getMessage());
           }
@@ -72,7 +110,7 @@ public class AndroidMpdClient extends Activity {
           Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
           intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                           RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-          intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+          intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Search by artist:");
           startActivityForResult(intent, RECOGNIZER_REQUEST_CODE);
 
           
@@ -86,14 +124,19 @@ public class AndroidMpdClient extends Activity {
     if (requestCode == RECOGNIZER_REQUEST_CODE && resultCode == RESULT_OK) {
       List<String> matches = data.getStringArrayListExtra(
         RecognizerIntent.EXTRA_RESULTS);
+      // List<Float> scores = data.getFloatArrayListExtra(
+      //  RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
 
+//      int i = 0;
       for (String match : matches) {
         Log.v("AndroidMpdClient - Matches ***** ", match);
+//        Log.v("AndroidMpdClient - SCORE: ", scores.get(i++));
       }
 
       try {
         TextView statusText = (TextView) findViewById(R.id.status_text);
         statusText.setText("Searching: " + matches.get(0));
+
 
         List<MPDSong> songs = new ArrayList<MPDSong>(
           mpd.getMPDDatabase().searchArtist(matches.get(0)));
