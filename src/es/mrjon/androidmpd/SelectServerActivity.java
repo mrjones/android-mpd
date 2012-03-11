64ac1d04dd8ae21d0aba35293fd60b94b5006d79
Matt Jones <jonesmr+github@gmail.com>
2012-03-11 23:19:36 +0000

    added rough skeleton (layout, intents) for server selection activity

--- src/es/mrjon/androidmpd/SelectServerActivity.java
+++ src/es/mrjon/androidmpd/SelectServerActivity.java
@@ -1,12 +1,27 @@
 package es.mrjon.androidmpd;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SelectServerActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.select_server);
+
+    Button saveButton = (Button) findViewById(R.id.save_settings_button);
+    final SelectServerActivity thisActivity = this;
+    saveButton.setOnClickListener(new OnClickListener() {
+        public void onClick(View v) {
+          Intent intent = thisActivity.getIntent();
+          thisActivity.finish();
+        }
+      });
+
+    
   }
 }
